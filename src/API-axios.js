8b7e1dafe554c50a3bdfbcf470c3971f5278bd26
Ferran Pérez Pastor <>
2024-11-15 21:02:13 +0100

    Modificando otra vez la url de la llamada a la api

--- src/API-axios.js
+++ src/API-axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 
 function APIAxios() {
   const [tasks, setTasks] = useState([]);
-  const API_URL =
-    process.env.REACT_APP_API_URL || "http://localhost:5000/tasks";
+  const API_URL = "http://localhost:5000/tasks";
 
   // Leer tareas
   useEffect(() => {
